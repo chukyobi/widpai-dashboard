dbb9864c1ce57f1bc12a715468bcf8c83fe253cb
chukyobi <josephclinton.obi@gmail.com>
2026-05-21 11:34:47 -0700

    Fix proxy export name for Next 16

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import { getSessionFromRequest } from './lib/auth'
 
 const PUBLIC_PATHS = ['/login', '/signup', '/forgot-password']
 
-export async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
   const isPublic = PUBLIC_PATHS.some(p => pathname.startsWith(p))
   const session = await getSessionFromRequest(request)
